1c4474cfd28461f193922821f4ca330e71994e95
makopi23 <kipersonalpc@gmail.com>
2014-10-22 02:35:35 +0900

    add chapter17 and chapter18

---
+++ chapter17_Observer/observer/sample/DigitObserver.java
@@ -0,0 +1,16 @@
+package observer.sample;
+
+public class DigitObserver implements Observer {
+
+	@Override
+	public void update(NumberGenerator generator) {
+		System.out.println("DigitObserver:" + generator.getNumber());
+		try{
+			Thread.sleep(100);
+		}catch(InterruptedException e){
+			// 何もしない
+		}
+
+	}
+
+}
