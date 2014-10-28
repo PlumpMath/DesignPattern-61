d424793ff0b6b42c92f6c5edb7d589bc75675572
makopi23 <kipersonalpc@gmail.com>
2014-10-29 03:15:47 +0900

    chapter20 completed.

---
+++ chapter20_Flyweight/flyweight/sample/BigCharFactory.java
@@ -0,0 +1,22 @@
+package flyweight.sample;
+
+import java.util.HashMap;
+
+public class BigCharFactory {
+	private HashMap<String, BigChar> pool = new HashMap<String, BigChar>();
+	private static BigCharFactory singleton = new BigCharFactory();
+	private BigCharFactory(){
+		// 何もしない
+	}
+	public static BigCharFactory getInstance(){
+		return singleton;
+	}
+	public synchronized BigChar getBigChar(char charname){
+		BigChar bc = pool.get("" + charname);
+		if(bc == null){
+			bc = new BigChar(charname);
+			pool.put("" + charname, bc);
+		}
+		return bc;
+	}
+}
